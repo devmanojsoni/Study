d0badce4dee34da9f4c755babb330b695c04fc80
devmanojsoni <er.manojsoni@hotmail.com>
2018-02-08 19:57:39 +0530

    Commiting after staging changes

---
+++ algorithms/sorting/new file 2.cpp
@@ -0,0 +1,13 @@
+// Initially if a new file is added it is not staged
+// Either Add from bash or VSCode
+
+// Bash
+// $ git add <file> // Add file(s) to index
+
+// VSCode
+// Click +, U -->> A, Stage File
+// Click -, A -->> U, to unstage
+// Save First = Ctrl+S,
+// if you see M, that means its modified
+// Stage Changes by clicking +
+// Commit Changes and Push
