f01bcd2ca3d6ef0d4ede8197287f13ec8f1d265c
devmanojsoni <er.manojsoni@hotmail.com>
2018-02-08 15:08:06 +0530

    Selection Sort
    Added selection sort algorithm

---
+++ algorithms/sorting/SelectionSort.cpp
@@ -0,0 +1,72 @@
+/* Example of Selection Sort Algorithm
+   Time complexity of Selection Sort is o(n^2)
+   i.e Big Oh of n-Square (say)
+   Big Oh of n-square is not considered the
+   best running time for an algorithm.
+   Therefore, Selection Sort is Slow
+*/
+#include<iostream>
+using namespace std;
+
+/* Uncomment this function if you wish to use this
+  void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+    }
+*/
+
+void SelectionSort(int A[], int n){
+    for(int i=0; i<(n-2); i++){ // Enter the first pass
+        int iMin = i;// Say i(th) is the minimum element,
+        // In first case say index 0 is the minimum
+        
+        /* Start from (i+1)th index, 
+           and go all the way upto length of the left part
+           of the array (say, from i+1th all the way upto nth)
+        */
+        for(int j=i+1; j<n; j++){ // Iteration
+            if(A[j] < A[iMin]) // Compare
+            /* Even though the iteration starts from
+               i+1th index, but it 
+               compare the 0th index to 1th index also
+            */
+            
+            // Then (If you found the minimum) 
+            iMin = j; // update the index of minimum element
+        }
+        /* Simple swapping between
+           A[i] and A[iMin]
+        */
+        int temp = A[i];
+        A[i] = A[iMin];
+        A[iMin] = temp;
+        /*
+            // Or use this call function here
+            swap(&arr[min_idx], &arr[i]);
+            
+            // if you wish to use the above function
+            // uncomment the swap function
+        */
+    }
+}
+int main(){
+    int A[] = {7,5,4,1,2,3};
+    SelectionSort(A,6);
+    for(int i=0; i<6; i++)
+    cout<<A[i]<<" ";
+}
+
+/* References
+  MyCodeSchool
+  https://www.youtube.com/watch?v=GUDLRan2DWM
+  
+  
+  Algorithm World
+  https://www.youtube.com/watch?v=s5P3uZBexKM
+  
+  
+
+  
+  
+*/
